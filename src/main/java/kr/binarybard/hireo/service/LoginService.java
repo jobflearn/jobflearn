a66de05f504a537e9259d547b7e9e511e8016838
GangminRYOU <dbrkdals1994@gmail.com>
2023-06-06 14:53:34 +0900

    fix : 검증 메소드를 PasswordEncoder의 matches로 변경

--- src/main/java/kr/binarybard/hireo/service/LoginService.java
+++ src/main/java/kr/binarybard/hireo/service/LoginService.java
@@ -4,6 +4,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kr.binarybard.hireo.domain.Member;
+import kr.binarybard.hireo.domain.MemberDto;
 import kr.binarybard.hireo.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,13 +16,12 @@ public class LoginService {
 	private final MemberRepository memberRepository;
 	private final PasswordEncoder passwordEncoder;
 
-	public Boolean login(Member member) {
-		member.getEncodedPassword(passwordEncoder);
-		Member found = memberRepository.findOneByEmail(member.getEmail());
-		if (found.getPassword().equals(member.getPassword())) {
+	public Boolean login(MemberDto memberDto) {
+		Member found = memberRepository.findOneByEmail(memberDto.getEmail());
+		if (passwordEncoder.matches(memberDto.getPassword(), found.getPassword())) {
 			return true;
 		} else {
-			return false;
+			throw new IllegalStateException("비밀번호가 틀립니다!");
 		}
 	}
 }
