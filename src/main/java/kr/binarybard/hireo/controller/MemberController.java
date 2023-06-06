8586708020ca833d159ce292bf31e4ef91e7aa86
GangminRYOU <dbrkdals1994@gmail.com>
2023-06-06 14:54:54 +0900

    feat : MemberController프로토 타입

--- src/main/java/kr/binarybard/hireo/controller/MemberController.java
+++ src/main/java/kr/binarybard/hireo/controller/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import kr.binarybard.hireo.domain.Member;
+import kr.binarybard.hireo.domain.MemberDto;
 import kr.binarybard.hireo.service.LoginService;
 import kr.binarybard.hireo.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -25,8 +26,8 @@ public class MemberController {
 	}
 
 	@PostMapping("/register")
-	public String registerMember(@ModelAttribute Member member) {
-		memberService.join(member);
+	public String registerMember(@ModelAttribute MemberDto memberDto) {
+		memberService.join(memberDto);
 		return "redirect:/home";
 	}
 
@@ -36,11 +37,11 @@ public class MemberController {
 	}
 
 	@PostMapping("/login")
-	public String login(@ModelAttribute Member member, HttpServletRequest request, Model model) {
-		if (loginService.login(member)) {
+	public String login(@ModelAttribute MemberDto memberDto, HttpServletRequest request, Model model) {
+		if (loginService.login(memberDto)) {
 			HttpSession session = request.getSession();
-			session.setAttribute("email", member.getEmail());
-			Member loginnedMember = memberService.findMemberByEmail(member.getEmail());
+			session.setAttribute("email", memberDto.getEmail());
+			Member loginnedMember = memberService.findMemberByEmail(memberDto.getEmail());
 			model.addAttribute("member", loginnedMember);
 			return "/home";
 		} else {
