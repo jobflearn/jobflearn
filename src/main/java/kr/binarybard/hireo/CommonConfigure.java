543bc7d9fe7405eef1553afffcbb16a02c5c7ae4
GangminRYOU <dbrkdals1994@gmail.com>
2023-06-06 14:56:04 +0900

    feat : MemberMapper 스프링 빈 등록

--- src/main/java/kr/binarybard/hireo/CommonConfigure.java
+++ src/main/java/kr/binarybard/hireo/CommonConfigure.java
@@ -5,10 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import kr.binarybard.hireo.domain.MemberMapper;
+
 @Configuration
 public class CommonConfigure {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	public MemberMapper memberMapper() {
+		return MemberMapper.INSTANCE;
 	}
 }
