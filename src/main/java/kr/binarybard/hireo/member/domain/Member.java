592823ce11a3dc0303399a319b86646ee86d4a41
GangminRYOU <dbrkdals1994@gmail.com>
2023-06-19 19:36:19 +0900

    HIR-12 feat : Member객체 연관관계 설정 - Member-Company 다대일 관계설정

--- src/main/java/kr/binarybard/hireo/member/domain/Member.java
+++ src/main/java/kr/binarybard/hireo/member/domain/Member.java
@@ -1,14 +1,18 @@
 package kr.binarybard.hireo.member.domain;
 
-import lombok.Builder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import kr.binarybard.hireo.company.domain.Company;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,6 +27,9 @@ public class Member extends BaseTimeEntity {
 	@Id
 	@GeneratedValue
 	private Long id;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "company_id")
+	private Company company;
 
 	private String email;
 	private String password;
@@ -37,6 +44,10 @@ public class Member extends BaseTimeEntity {
 		this.password = password;
 		this.role = role;
 		this.name = name;
+	}
+
+	public void changeCompany(Company company) {
+		this.company = company;
 	}
 
 	public void changeRole(Role role) {
