ee3541c6a17ee627c51c62f3d1a9daca2383fc89
GangminRYOU <dbrkdals1994@gmail.com>
2023-06-19 19:38:21 +0900

    HIR-12 test : CompanyService 클래스테스트를 위해 Fixture추가

---
+++ src/test/java/kr/binarybard/hireo/fixture/LocationFixture.java
@@ -0,0 +1,13 @@
+package kr.binarybard.hireo.fixture;
+
+import kr.binarybard.hireo.location.domain.Address;
+import kr.binarybard.hireo.location.domain.Location;
+
+public class LocationFixture {
+	public static final Location TEST_LOCATION = Location.builder()
+		.latitude(37.4493638197085)
+		.longitude(127.1275005197085)
+		.address(Address.builder().city("안양").province("경기도").street("관악로 32").build())
+		.countryCode("KR")
+		.build();
+}
