4628a8dbeb39ca67468eda506cee7c6c98cd5532
BINARYBARD <binarybard@pm.me>
2023-06-20 01:45:40 +0900

    chore: JWT 관련 시큐리티 설정 추가

--- src/main/java/kr/binarybard/hireo/config/SecurityConfig.java
+++ src/main/java/kr/binarybard/hireo/config/SecurityConfig.java
@@ -2,6 +2,10 @@ package kr.binarybard.hireo.config;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.*;
 
+import kr.binarybard.hireo.config.jwt.JwtAccessDeniedHandler;
+import kr.binarybard.hireo.config.jwt.JwtAuthTokenFilter;
+import kr.binarybard.hireo.config.jwt.JwtAuthenticationEntryPoint;
+import kr.binarybard.hireo.config.jwt.JwtTokenProvider;
 import kr.binarybard.hireo.config.oauth.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -16,12 +20,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 	private final CustomOAuth2UserService customOAuth2UserService;
+	private final JwtTokenProvider jwtTokenProvider;
+	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -43,8 +51,12 @@ public class SecurityConfig {
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/api/auth/**").permitAll()
 				.anyRequest().authenticated())
+			.exceptionHandling(handler -> handler
+				.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+				.accessDeniedHandler(jwtAccessDeniedHandler))
 			.sessionManagement(session -> session
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.addFilterBefore(new JwtAuthTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
 			.build();
 	}
 
