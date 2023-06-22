487e9daa552fff3da57bfb48569060b5083d94a8
GangminRYOU <dbrkdals1994@gmail.com>
2023-06-22 18:45:31 +0900

    HIR-12 feat : CompanyController추가 및 CompanyControllerTest추가

--- src/main/java/kr/binarybard/hireo/web/company/controller/CompanyController.java
+++ src/main/java/kr/binarybard/hireo/web/company/controller/CompanyController.java
@@ -1,14 +1,23 @@
 package kr.binarybard.hireo.web.company.controller;
 
-import kr.binarybard.hireo.exception.CompanyNotFoundException;
-import kr.binarybard.hireo.web.company.service.CompanyService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.binarybard.hireo.common.CurrentUser;
+import kr.binarybard.hireo.exception.CompanyNotFoundException;
+import kr.binarybard.hireo.web.company.dto.CompanyRegister;
+import kr.binarybard.hireo.web.company.dto.CompanyResponse;
+import kr.binarybard.hireo.web.company.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("/companies")
 @RequiredArgsConstructor
--- src/test/java/kr/binarybard/hireo/web/company/controller/CompanyControllerTest.java
+++ src/test/java/kr/binarybard/hireo/web/company/controller/CompanyControllerTest.java
@@ -1,8 +1,13 @@
 package kr.binarybard.hireo.web.company.controller;
 
-import kr.binarybard.hireo.exception.CompanyNotFoundException;
-import kr.binarybard.hireo.web.company.service.CompanyService;
-import kr.binarybard.hireo.web.fixture.CompanyResponseFixture;
+import static kr.binarybard.hireo.web.fixture.CompanyFixture.*;
+import static kr.binarybard.hireo.web.fixture.CompanyResponseFixture.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,15 +15,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
-import static kr.binarybard.hireo.web.fixture.CompanyFixture.EXISTING_COMPANY_ID;
-import static kr.binarybard.hireo.web.fixture.CompanyFixture.NON_EXISTING_COMPANY_ID;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import kr.binarybard.hireo.exception.CompanyNotFoundException;
+import kr.binarybard.hireo.web.company.dto.CompanyRegister;
+import kr.binarybard.hireo.web.company.service.CompanyService;
 
 @WithMockUser
 @SpringBootTest
@@ -33,9 +39,43 @@ class CompanyControllerTest {
 
 	@BeforeEach
 	void setup() {
-		var companyResponse = CompanyResponseFixture.TEST_COMPANY_RESPONSE;
-		when(companyService.findOne(EXISTING_COMPANY_ID)).thenReturn(companyResponse);
+		when(companyService.findOne(EXISTING_COMPANY_ID)).thenReturn(TEST_COMPANY_RESPONSE);
 		when(companyService.findOne(NON_EXISTING_COMPANY_ID)).thenThrow(CompanyNotFoundException.class);
+	}
+
+	@Test
+	@DisplayName("회사등록 폼 요청")
+	void requestCompanyRegisterFormTest() throws Exception {
+		mockMvc.perform(get("/companies/new"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(view().name("company/company-form"));
+	}
+
+	@Test
+	@DisplayName("회사 등록 요청")
+	void registerCompanyTest() throws Exception {
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("name", "gangmin");
+		params.add("description", "hahahah");
+		params.add("isVerified", "true");
+		params.add("industry", "IT");
+		params.add("latitude", "13.2323");
+		params.add("longitude", "123.2323");
+		params.add("countryCode", "KR");
+		params.add("province", "kyeongi");
+		params.add("city", "anyang");
+		params.add("street", "wondol");
+		params.add("district", "gogildong");
+		params.add("premise", "www");
+		mockMvc.perform(post("/companies/new")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.params(params))
+			.andDo(print())
+			.andExpect(status().is3xxRedirection())
+			.andExpect(redirectedUrl("/"));
+
+		verify(companyService).registerCompany(any(CompanyRegister.class), any(User.class));
 	}
 
 	@Test
