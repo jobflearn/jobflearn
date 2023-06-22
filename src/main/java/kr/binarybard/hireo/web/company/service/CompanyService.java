3cbf822d533e6bf4bf2a3c7e16ea432749389a10
Yousilience <115459147+GangminRYOU@users.noreply.github.com>
2023-06-22 23:03:13 +0900

    HIR-12 feat: 고용주가 자신의 회사에 대한 정보를 등록할 수 있는 기능 추가 (#18)
    * HIR-12 feat : Company, Location 엔티티 추가 및 Member 엔티티 연관관계 수정
     - Address 임베디드 타입 객체
     - Member-Company 다대일 관계
     - Company-Location 일대일 관계
    
    * HIR-12 feat : Location, Company 엔티티 매퍼 추가 및 DTO 클래스 추가
    
    * HIR-12 feat : CompanyRepository, LocationRepository추가
    - 스프링 데이터 JPA 활용
    
    * HIR-12 feat : Member객체 연관관계 설정
    - Member-Company 다대일 관계설정
    
    * HIR-12 feat : CompanyService 클래스 추가
    - save
    - findById
    
    * HIR-12 test : CompanyService 클래스테스트를 위해 Fixture추가
    
    * HIR-12 test : CompanyService 클래스테스트 추가
    
    * HIR-12 rename : git 충돌 방지를 위해 디렉토리 구조 수정
    
    * HIR-12 docs : 회사 등록 폼 수정
    
    * HIR-12 refactor : CompanyDTO, LocationDto수정, MemberEntity 수정, industry값타입 객체 수정
    
    * HIR-12 refactor : Company엔티티 수정
    
    * HIR-12 refactor : CompanyService수정
    
    * HIR-12 refactor : CompanyServiceTest 수정
    
    * HIR-12 feat : CompanyController추가 및 CompanyControllerTest추가
    
    * HIR-12 docs : data.sql수정, home.html 수정
    
    * HIR-12 test : MemberFixture추가
    
    * HIR-12 refactor : LocationService수정
    
    * HIR-12 feat : LocationNotFoundException추가
    
    * HIR-12 feat : CurrentUser Annotation추가 - 회사 등록 기능시 사용
    
    * HIR-12 refactor : 테스트 라이브러리 추가
    
    * HIR-12 fix : @ModelAttribute - Setter추가
    
    * HIR-12 chore : H2 데이터베이스 관련 설정 추가
    
    * HIR-12 test : MemberFixture, CompanyServiceTest 복구
    
    * HIR-12 feat : CompanyController 추가

--- src/main/java/kr/binarybard/hireo/web/company/service/CompanyService.java
+++ src/main/java/kr/binarybard/hireo/web/company/service/CompanyService.java
@@ -1,31 +1,44 @@
 package kr.binarybard.hireo.web.company.service;
 
-import kr.binarybard.hireo.common.exceptions.EntityNotFoundException;
-import kr.binarybard.hireo.common.exceptions.ErrorCode;
-import kr.binarybard.hireo.web.company.dto.CompanyResponse;
+import java.util.Locale;
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import kr.binarybard.hireo.common.exceptions.EntityNotFoundException;
+import kr.binarybard.hireo.common.exceptions.ErrorCode;
 import kr.binarybard.hireo.web.company.domain.Company;
 import kr.binarybard.hireo.web.company.dto.CompanyMapper;
 import kr.binarybard.hireo.web.company.dto.CompanyRegister;
+import kr.binarybard.hireo.web.company.dto.CompanyResponse;
 import kr.binarybard.hireo.web.company.repository.CompanyRepository;
+import kr.binarybard.hireo.web.location.dto.LocationMapper;
+import kr.binarybard.hireo.web.member.domain.Member;
+import kr.binarybard.hireo.web.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-
-import java.util.Locale;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CompanyService {
 
 	private final CompanyRepository companyRepository;
+	private final MemberRepository memberRepository;
 	private final CompanyMapper companyMapper;
+	private final LocationMapper locationMapper;
 	private final MessageSource messageSource;
 
-	public Long registerCompany(CompanyRegister companyRegister) {
+	@Transactional
+	public CompanyResponse registerCompany(CompanyRegister companyRegister, User user) {
 		Company company = companyMapper.toEntity(companyRegister);
-		return companyRepository.save(company).getId();
+		Member member = memberRepository.findByEmail(user.getUsername())
+			.orElseThrow(() -> new EntityNotFoundException(ErrorCode.MEMBER_NOT_FOUND));
+		member.changeCompany(company);
+		return companyMapper.toDto(companyRepository.save(company));
 	}
 
 	public Company findById(Long id) {
