6414eb34723f80ca4507de75dd334104e4114c86
BINARYBARD <binarybard@pm.me>
2023-06-23 01:48:26 +0900

    refactor: 자주 반복되는 메서드를 리포지토리에 디폴트 메서드로 추가

--- src/main/java/kr/binarybard/hireo/web/company/repository/CompanyRepository.java
+++ src/main/java/kr/binarybard/hireo/web/company/repository/CompanyRepository.java
@@ -1,8 +1,14 @@
 package kr.binarybard.hireo.web.company.repository;
 
+import kr.binarybard.hireo.common.exceptions.EntityNotFoundException;
+import kr.binarybard.hireo.common.exceptions.ErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kr.binarybard.hireo.web.company.domain.Company;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+	default Company findByIdOrThrow(Long id) {
+		return findById(id).orElseThrow(() -> new EntityNotFoundException(ErrorCode.COMPANY_NOT_FOUND, id.toString()));
+	}
 }
