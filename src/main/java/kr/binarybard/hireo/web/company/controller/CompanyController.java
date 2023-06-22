500d9ca9677389cbe8e3b17cec0f161609c62388
GangminRYOU <dbrkdals1994@gmail.com>
2023-06-22 19:42:42 +0900

    HIR-12 feat : CompanyController 추가

--- src/main/java/kr/binarybard/hireo/web/company/controller/CompanyController.java
+++ src/main/java/kr/binarybard/hireo/web/company/controller/CompanyController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.binarybard.hireo.api.common.exceptions.EntityNotFoundException;
 import kr.binarybard.hireo.common.CurrentUser;
-import kr.binarybard.hireo.exception.CompanyNotFoundException;
 import kr.binarybard.hireo.web.company.dto.CompanyRegister;
 import kr.binarybard.hireo.web.company.dto.CompanyResponse;
 import kr.binarybard.hireo.web.company.service.CompanyService;
@@ -24,7 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 public class CompanyController {
 	private final CompanyService companyService;
 
-	@GetMapping("/{id}")
+	@GetMapping("/{id:\\d+}")
 	public String profile(
 		@PathVariable("id") Long id,
 		Model model
@@ -33,8 +33,23 @@ public class CompanyController {
 			var foundCompany = companyService.findOne(id);
 			model.addAttribute("company", foundCompany);
 			return "company/profile";
-		} catch (CompanyNotFoundException ex) {
+		} catch (EntityNotFoundException ex) {
 			return "redirect:/error/404";
 		}
+	}
+
+	@GetMapping("/new")
+	public String registerForm(Model model) {
+		var company = CompanyRegister.builder().build();
+		model.addAttribute("company", company);
+		return "company/company-form";
+	}
+
+	@PostMapping("/new")
+	public String registerCompany(@CurrentUser User user, @ModelAttribute CompanyRegister companyRegister,
+		Model model) {
+		CompanyResponse companyResponse = companyService.registerCompany(companyRegister, user);
+		model.addAttribute("company", companyResponse);
+		return "redirect:/";
 	}
 }
