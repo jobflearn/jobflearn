2200069f4835811d2a4a8afda577b9721ddc06b2
GangminRYOU <dbrkdals1994@gmail.com>
2023-06-22 18:46:36 +0900

    HIR-12 test : MemberFixture추가

--- src/test/java/kr/binarybard/hireo/web/fixture/MemberFixture.java
+++ src/test/java/kr/binarybard/hireo/web/fixture/MemberFixture.java
@@ -4,6 +4,7 @@ import kr.binarybard.hireo.web.member.domain.Member;
 import kr.binarybard.hireo.web.member.domain.Role;
 
 public class MemberFixture {
+	public static final long EXISTING_MEMBER_ID = 1L;
 	public static final Member TEST_MEMBER = Member.builder()
 		.email("test@test.com")
 		.password("1234")
