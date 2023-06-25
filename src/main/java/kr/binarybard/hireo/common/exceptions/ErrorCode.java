10f480b9cf8361a564a89186b9d35ffc60a6340c
BINARYBARD <binarybard@pm.me>
2023-06-25 11:39:06 +0900

    HIR-32 feat: 서버에 파일을 업로드 하는 기능 추가 (#21)
    * HIR-32 feat: 파일을 저장하거나 서빙하는 기능 추가
    
    * HIR-32 chore: 파일 처리의 상태를 나타내는 에러 코드 추가
    
    * HIR-32 test: 파일 서비스의 단위 테스트 추가
    
    * HIR-32 feat: 파일 처리 중 발생하는 예외를 나타내는 클래스 추가
    
    * HIR-32 feat: 파일을 업로드하거나 서빙하는 엔드포인트 제공
    
    * HIR-32 test: 파일 API 컨트롤러에 대한 인수 테스트 추가
    
    * HIR-32 test: REST Docs 설정 클래스 추가
    
    * HIR-32 config: API 문서에 대한 접근 권한을 개방함
    
    * HIR-32 build: REST Docs 의존성 추가
    
    * HIR-32 test: 인증 API에 관한 문서화 테스트 추가
    
    * HIR-32 test: 파일 API에 대한 문서화 테스트 추가

--- src/main/java/kr/binarybard/hireo/common/exceptions/ErrorCode.java
+++ src/main/java/kr/binarybard/hireo/common/exceptions/ErrorCode.java
@@ -22,6 +22,14 @@ public enum ErrorCode {
 	INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "A_001", "유효하지 않은 토큰입니다."),
 	UNSUPPORTED_OAUTH2_PROVIDER(HttpStatus.BAD_REQUEST, "A_002", "지원하지 않는 OAuth2 프로바이더입니다."),
 
+	FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "F_001", "파일 업로드 중 오류가 발생하였습니다."),
+	FILE_HASHING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "F_002", "파일 해싱 중 오류가 발생하였습니다."),
+	FILE_READING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "F_003", "파일 읽기 중 오류가 발생하였습니다."),
+	FILE_STORAGE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "F_004", "파일 저장 중 오류가 발생하였습니다."),
+	FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "F_005", "파일을 찾을 수 없습니다."),
+	FILE_SIZE_TOO_LARGE(HttpStatus.BAD_REQUEST, "F_006", "파일 크기가 너무 큽니다."),
+	FILE_SIZE_ZERO(HttpStatus.BAD_REQUEST, "F_007", "파일 크기가 0입니다."),
+
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S_001", "서버에 오류가 발생하였습니다."),
 	INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "S_002", "잘못된 요청 값입니다."),
 	TOKEN_HASHING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S_003", "토큰 해싱 중 오류가 발생하였습니다.");
---
+++ src/test/java/kr/binarybard/hireo/api/docs/AuthenticationApiControllerTest.java
@@ -0,0 +1,124 @@
+package kr.binarybard.hireo.api.docs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.binarybard.hireo.api.auth.dto.RefreshTokenRequest;
+import kr.binarybard.hireo.api.auth.dto.SignInRequest;
+import kr.binarybard.hireo.api.auth.service.RefreshTokenService;
+import kr.binarybard.hireo.config.jwt.JwtTokenProvider;
+import kr.binarybard.hireo.web.auth.dto.SignUpRequest;
+import kr.binarybard.hireo.web.auth.service.LoginService;
+import kr.binarybard.hireo.web.member.service.MemberService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class AuthenticationApiControllerTest extends RestDocsConfiguration {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@MockBean
+	private LoginService loginService;
+
+	@MockBean
+	private JwtTokenProvider jwtTokenProvider;
+
+	@MockBean
+	private AuthenticationManagerBuilder authenticationManagerBuilder;
+
+	@Mock
+	private AuthenticationManager authenticationManager;
+
+	@MockBean
+	private RefreshTokenService refreshTokenService;
+
+	private final String username = "test@example.com";
+	private final String password = "password1234";
+
+	@BeforeEach
+	void setup() {
+		when(jwtTokenProvider.createAccessToken(any())).thenReturn("valid-access-token");
+		when(jwtTokenProvider.createRefreshToken(any())).thenReturn("valid-refresh-token");
+		when(refreshTokenService.validateToken(anyString())).thenReturn(true);
+		when(jwtTokenProvider.getUsernameFromToken(anyString())).thenReturn(username);
+		when(loginService.loadUserByUsername(anyString())).thenReturn(new User(username, password, new ArrayList<>()));
+
+		when(authenticationManagerBuilder.getObject()).thenReturn(authenticationManager);
+	}
+
+	@Test
+	@DisplayName("로그인 성공")
+	void loginSuccess() throws Exception {
+		// Arrange
+		SignInRequest signInRequest = SignInRequest.builder()
+			.email(username)
+			.password(password)
+			.build();
+
+		// Act & Assert
+		mockMvc.perform(post("/api/auth/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(signInRequest)))
+			.andExpect(status().isOk())
+			.andDo(document("login",
+				requestFields(
+					fieldWithPath("email").description("로그인 이메일"),
+					fieldWithPath("password").description("로그인 비밀번호")
+				),
+				responseFields(
+					fieldWithPath("access_token").description("액세스 토큰"),
+					fieldWithPath("refresh_token").description("리프레시 토큰")
+				)
+			));
+	}
+
+	@Test
+	@DisplayName("토큰 재발급 성공")
+	void reissueTokenSuccess() throws Exception {
+		// Arrange
+		RefreshTokenRequest refreshTokenRequest = RefreshTokenRequest.builder()
+			.refreshToken("refresh_token")
+			.build();
+
+		// Act & Assert
+		mockMvc.perform(post("/api/auth/reissue")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(refreshTokenRequest)))
+			.andExpect(status().isOk())
+			.andDo(document("reissue-token",
+				requestFields(
+					fieldWithPath("refresh_token").description("재발급 요청 토큰")
+				),
+				responseFields(
+					fieldWithPath("access_token").description("액세스 토큰"),
+					fieldWithPath("refresh_token").description("리프레시 토큰")
+				)
+			));
+	}
+}
