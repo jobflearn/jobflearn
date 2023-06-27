aaa0cbf93ed7cf95cabba82615a54291d4be9273
BINARYBARD <binarybard@pm.me>
2023-06-27 19:31:48 +0900

    HIR-32 test: 새로 추가된 서비스 계층에 대한 단위 테스트 수행

---
+++ src/test/java/kr/binarybard/hireo/api/auth/service/JwtLoginServiceTest.java
@@ -0,0 +1,62 @@
+package kr.binarybard.hireo.api.auth.service;
+
+import kr.binarybard.hireo.api.auth.dto.SignInRequest;
+import kr.binarybard.hireo.api.auth.dto.TokenResponse;
+import kr.binarybard.hireo.common.fixture.MemberFixture;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class JwtLoginServiceTest {
+
+	@Mock
+	private AuthenticationManagerBuilder authenticationManagerBuilder;
+
+	@Mock
+	private JwtTokenService tokenService;
+
+	@Mock
+	private AuthenticationManager authenticationManager;
+
+	@Mock
+	private Authentication authentication;
+
+	@InjectMocks
+	private JwtLoginService jwtLoginService;
+
+	@Test
+	@DisplayName("사용자 인증 후 JWT 토큰을 생성한다.")
+	void authenticateUserTest() {
+		// given
+		String testAccessToken = "testAccessToken";
+		String testRefreshToken = "testRefreshToken";
+		SignInRequest request = MemberFixture.SIGNIN_REQUEST_MEMBER;
+
+		when(authenticationManagerBuilder.getObject()).thenReturn(authenticationManager);
+		when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+			.thenReturn(authentication);
+		when(tokenService.generateJwtTokens(anyString(), any(Authentication.class)))
+			.thenReturn(new TokenResponse(testAccessToken, testRefreshToken));
+
+		// when
+		TokenResponse response = jwtLoginService.authenticateUser(request);
+
+		// then
+		assertThat(response.accessToken()).isEqualTo(testAccessToken);
+		assertThat(response.refreshToken()).isEqualTo(testRefreshToken);
+		assertThat(SecurityContextHolder.getContext().getAuthentication()).isEqualTo(authentication);
+	}
+}
+
--- src/test/java/kr/binarybard/hireo/api/docs/AuthenticationApiControllerTest.java
+++ src/test/java/kr/binarybard/hireo/api/docs/AuthenticationApiControllerTest.java
@@ -5,19 +5,16 @@ import kr.binarybard.hireo.api.auth.dto.RefreshTokenRequest;
 import kr.binarybard.hireo.api.auth.dto.SignInRequest;
 import kr.binarybard.hireo.api.auth.service.RefreshTokenService;
 import kr.binarybard.hireo.config.jwt.JwtTokenProvider;
-import kr.binarybard.hireo.web.auth.dto.SignUpRequest;
 import kr.binarybard.hireo.web.auth.service.LoginService;
-import kr.binarybard.hireo.web.member.service.MemberService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,12 +24,9 @@ import java.util.ArrayList;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
-import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class AuthenticationApiControllerTest extends RestDocsConfiguration {
@@ -68,7 +62,7 @@ class AuthenticationApiControllerTest extends RestDocsConfiguration {
 		when(refreshTokenService.validateToken(anyString())).thenReturn(true);
 		when(jwtTokenProvider.getUsernameFromToken(anyString())).thenReturn(username);
 		when(loginService.loadUserByUsername(anyString())).thenReturn(new User(username, password, new ArrayList<>()));
-
+		when(jwtTokenProvider.getAuthentication(anyString())).thenReturn(new UsernamePasswordAuthenticationToken(username, null, new ArrayList<>()));
 		when(authenticationManagerBuilder.getObject()).thenReturn(authenticationManager);
 	}
 
@@ -107,7 +101,7 @@ class AuthenticationApiControllerTest extends RestDocsConfiguration {
 			.build();
 
 		// Act & Assert
-		mockMvc.perform(post("/api/auth/reissue")
+		mockMvc.perform(post("/api/auth/refresh")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsString(refreshTokenRequest)))
 			.andExpect(status().isOk())
