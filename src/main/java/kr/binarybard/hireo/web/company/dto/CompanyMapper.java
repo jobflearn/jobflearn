c3ff596ddaec917b0ef3a40a784648fa4a82dff1
GangminRYOU <dbrkdals1994@gmail.com>
2023-06-27 17:34:11 +0900

    HIR-12 chores : 불필요한 ImageHashUtils.java 제거 및 CompanyMapper수정

--- src/main/java/kr/binarybard/hireo/web/company/dto/CompanyMapper.java
+++ src/main/java/kr/binarybard/hireo/web/company/dto/CompanyMapper.java
@@ -1,14 +1,9 @@
 package kr.binarybard.hireo.web.company.dto;
 
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
-import kr.binarybard.hireo.common.exceptions.ErrorCode;
-import kr.binarybard.hireo.common.exceptions.FileProcessingException;
-import kr.binarybard.hireo.utils.ImageHashUtils;
 import kr.binarybard.hireo.web.company.domain.Company;
 import kr.binarybard.hireo.web.location.dto.LocationMapper;
 
@@ -21,15 +16,7 @@ public interface CompanyMapper {
 
 	@Mapping(target = "locationDto", source = "location")
 	@Mapping(target = "countryName", ignore = true)
+	@Mapping(target = "logoHash", ignore = true)
 	CompanyResponse toDto(Company company);
 
-	@AfterMapping
-	default void mappingLogo(CompanyRegister companyRegister, @MappingTarget Company.CompanyBuilder company) {
-		try {
-			if (companyRegister.getCompanyLogo() != null)
-				company.logoHash(ImageHashUtils.hashImageFileWithMD5(companyRegister.getCompanyLogo()));
-		} catch (Exception e) {
-			throw new FileProcessingException(ErrorCode.FILE_HASHING_FAILED);
-		}
-	}
 }
