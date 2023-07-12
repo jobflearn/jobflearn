2542e814af8b0aeab579d466157d4e4df58fe6e6
GangminRYOU <dbrkdals1994@gmail.com>
2023-07-12 18:17:54 +0900

    HIR-47 test : 조건부 페이징 쿼리 테스트 추가

--- src/test/java/kr/binarybard/hireo/web/job/repository/JobRepositoryCustomImplTest.java
+++ src/test/java/kr/binarybard/hireo/web/job/repository/JobRepositoryCustomImplTest.java
@@ -1,18 +1,24 @@
 package kr.binarybard.hireo.web.job.repository;
 
-import java.util.List;
+import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
+import java.util.List;
+import java.util.Objects;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
+import kr.binarybard.hireo.common.fixture.LocationFixture;
+import kr.binarybard.hireo.web.job.domain.Category;
+import kr.binarybard.hireo.web.job.domain.JobType;
 import kr.binarybard.hireo.web.job.dto.JobListResponse;
+import kr.binarybard.hireo.web.job.dto.JobSearchCondition;
 
-@SpringBootTest
+@DataJpaTest
 class JobRepositoryCustomImplTest {
 
 	@Autowired
@@ -25,9 +31,99 @@ class JobRepositoryCustomImplTest {
 		PageRequest pageRequest = PageRequest.of(0, 4);
 		//when
 		Page<JobListResponse> jobListResponses = jobRepository.listJobs(pageRequest);
+		Page<JobListResponse> jobListResponses2 = jobRepository.listJobs(PageRequest.of(1, 4));
+		//then
+		System.out.println(jobListResponses.getNumber());
+		System.out.println(jobListResponses2.getNumber());
+
+		List<JobListResponse> content = jobListResponses.getContent();
+		assertThat(content).hasSize(4);
+		assertThat(jobListResponses.getNumber()).isZero();
+	}
+
+	@Test
+	@DisplayName("위치 조건에 따른 동적쿼리 페이징 테스트")
+	void listJobsWithConditionTest() throws Exception {
+		//given
+		JobSearchCondition simpleCondition = JobSearchCondition.builder()
+			.locationDto(LocationFixture.TEST_LOCATION_CONDITION_1)
+			.build();
+		PageRequest pageRequest = PageRequest.of(0, 4);
+		//when
+		Page<JobListResponse> jobListResponses = jobRepository.listJobsWithCondition(simpleCondition, pageRequest);
+
 		//then
 		List<JobListResponse> content = jobListResponses.getContent();
-		Assertions.assertThat(content.size()).isEqualTo(4);
-		Assertions.assertThat(jobListResponses.getNumber()).isEqualTo(0);
+		assertThat(content.stream().findAny()).isPresent();
+		assertThat(jobListResponses.getNumber()).isZero();
+	}
+
+	@Test
+	@DisplayName("키워드 조건에 따른 동적쿼리 페이징 테스트")
+	void listJobsWithKeywordTest() throws Exception {
+		//given
+		JobSearchCondition simpleCondition = JobSearchCondition.builder()
+			.keyword("소프트")
+			.build();
+		PageRequest pageRequest = PageRequest.of(0, 4);
+		//when
+		Page<JobListResponse> jobListResponses = jobRepository.listJobsWithCondition(simpleCondition, pageRequest);
+		List<JobListResponse> content1 = jobListResponses.getContent();
+		//then
+		List<JobListResponse> content = jobListResponses.getContent();
+		assertThat(Objects.requireNonNull(content.stream().findAny().orElse(null)).getName()).contains("소프트");
+		assertThat(jobListResponses.getNumber()).isZero();
+	}
+
+	@Test
+	@DisplayName("직무 조건에 따른 동적쿼리 페이징 테스트")
+	void listJobsWithCategoryTest() throws Exception {
+		//given
+		JobSearchCondition simpleCondition = JobSearchCondition.builder()
+			.category(Category.SALE_MAR)
+			.build();
+		PageRequest pageRequest = PageRequest.of(0, 4);
+		//when
+		Page<JobListResponse> jobListResponses = jobRepository.listJobsWithCondition(simpleCondition, pageRequest);
+
+		//then
+		List<JobListResponse> content = jobListResponses.getContent();
+		assertThat(content.stream().findAny().orElse(null).getCategory()).isEqualTo(Category.SALE_MAR);
+		assertThat(jobListResponses.getNumber()).isZero();
+	}
+
+	@Test
+	@DisplayName("근무 조건에 따른 동적쿼리 페이징 테스트")
+	void listJobsWithJobTypeTest() throws Exception {
+		//given
+		JobSearchCondition simpleCondition = JobSearchCondition.builder()
+			.jobType(JobType.FULLTIME)
+			.build();
+		PageRequest pageRequest = PageRequest.of(0, 4);
+		//when
+		Page<JobListResponse> jobListResponses = jobRepository.listJobsWithCondition(simpleCondition, pageRequest);
+
+		//then
+		List<JobListResponse> content = jobListResponses.getContent();
+		assertThat(Objects.requireNonNull(content.stream().findAny().orElse(null)).getJobType()).isEqualTo(
+			JobType.FULLTIME);
+		assertThat(jobListResponses.getNumber()).isZero();
+	}
+
+	@Test
+	@DisplayName("연봉 조건에 따른 동적쿼리 페이징 테스트")
+	void listJobsWithSalaryTest() throws Exception {
+		//given
+		JobSearchCondition simpleCondition = JobSearchCondition.builder()
+			.salaryRange("3300,9999")
+			.build();
+		PageRequest pageRequest = PageRequest.of(0, 4);
+		//when
+		Page<JobListResponse> jobListResponses = jobRepository.listJobsWithCondition(simpleCondition, pageRequest);
+
+		//then
+		List<JobListResponse> content = jobListResponses.getContent();
+		assertThat(content.stream().findAny()).isPresent();
+		assertThat(jobListResponses.getNumber()).isZero();
 	}
 }
