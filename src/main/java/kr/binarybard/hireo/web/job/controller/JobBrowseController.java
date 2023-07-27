ffc56f23ac1f7f2550980e157b4ae414b1d0d01b
GangminRYOU <dbrkdals1994@gmail.com>
2023-07-27 10:09:01 +0900

    HIR-59 refactor: JobBrowseController Rest로 수정

--- src/main/java/kr/binarybard/hireo/web/job/controller/JobBrowseController.java
+++ src/main/java/kr/binarybard/hireo/web/job/controller/JobBrowseController.java
@@ -2,20 +2,17 @@ package kr.binarybard.hireo.web.job.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import kr.binarybard.hireo.common.CurrentUser;
 import kr.binarybard.hireo.utils.DateUtils;
-import kr.binarybard.hireo.web.account.service.AccountService;
-import kr.binarybard.hireo.web.job.domain.JobType;
 import kr.binarybard.hireo.web.job.dto.JobListResponse;
+import kr.binarybard.hireo.web.job.dto.JobResponse;
 import kr.binarybard.hireo.web.job.dto.JobSearchCondition;
 import kr.binarybard.hireo.web.job.service.JobService;
 import lombok.RequiredArgsConstructor;
@@ -24,46 +21,31 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/jobs")
 @RequiredArgsConstructor
 public class JobBrowseController {
-	private final AccountService accountService;
 	private final JobService jobService;
 
-	@ModelAttribute("jobTypes")
-	public JobType[] itemTypes() {
-		/*배열로 넘겨준다.*/
-		return JobType.values();
-	}
-
 	@GetMapping("/{id}")
-	public String job(
-		@CurrentUser User user,
-		Model model,
-		@PathVariable Long id
-	) {
-		var foundJob = jobService.findOne(id);
-		model.addAttribute("company", foundJob.getCompany());
-		model.addAttribute("job", foundJob);
-		model.addAttribute("account", accountService.findByEmail(user.getUsername()));
-		return "job/info";
+	public ResponseEntity<JobResponse> job(@PathVariable Long id) {
+		return ResponseEntity.ok(jobService.findOne(id));
 	}
 
 	@GetMapping
-	public String jobList(@RequestParam(defaultValue = "1") Integer page, Model model) {
+	public ResponseEntity<Page<JobListResponse>> jobList(@RequestParam(defaultValue = "1") Integer page) {
 		Page<JobListResponse> jobListByPage = jobService.findByPage(PageRequest.of(page - 1, 4));
 		jobListByPage.stream().forEach(j -> j.setElapsedDate(DateUtils.getElapsedDateTime(j.getPostedAt())));
-		model.addAttribute("jobs", jobListByPage);
-		model.addAttribute("condition", new JobSearchCondition());
-		return "job/joblist";
+		return ResponseEntity.ok(jobListByPage);
 	}
 
 	@GetMapping("/search")
-	public String jobSearchCondition(
+	public ResponseEntity<Page<JobListResponse>> jobSearchCondition(
 		@RequestParam(defaultValue = "1") Integer page,
-		@ModelAttribute JobSearchCondition condition,
-		Model model) {
-		Page<JobListResponse> filteredList = jobService.findByPageWithCondition(condition,
-			PageRequest.of(page - 1, 4));
-		model.addAttribute("jobs", filteredList);
-		model.addAttribute("condition", new JobSearchCondition());
-		return "job/joblist";
+		@ModelAttribute JobSearchCondition condition
+	) {
+		return ResponseEntity
+			.ok(jobService
+				.findByPageWithCondition(
+					condition,
+					PageRequest.of(page - 1, 4)
+				));
+
 	}
 }
