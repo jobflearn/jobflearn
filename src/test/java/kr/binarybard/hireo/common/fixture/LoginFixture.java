746d2b8bc8f3e394f6e4db1e8c6adda42770f5e1
GangminRYOU <dbrkdals1994@gmail.com>
2023-07-27 10:19:26 +0900

    HIR-59 refactor: 중복 이메일 오류관련 수정

--- src/test/java/kr/binarybard/hireo/common/fixture/LoginFixture.java
+++ src/test/java/kr/binarybard/hireo/common/fixture/LoginFixture.java
@@ -1,7 +1,7 @@
 package kr.binarybard.hireo.common.fixture;
 
+import kr.binarybard.hireo.api.auth.dto.SignUpRequest;
 import kr.binarybard.hireo.web.account.domain.AccountType;
-import kr.binarybard.hireo.web.auth.dto.SignUpRequest;
 
 public class LoginFixture {
 	public static final SignUpRequest TEST_SIGNUP_REQUEST_JOBSEEKER = SignUpRequest.builder()
@@ -22,6 +22,14 @@ public class LoginFixture {
 
 	public static final SignUpRequest TEST_SIGNUP_REQUEST_ADMINISTRATOR = SignUpRequest.builder()
 		.email("admin@test.com")
+		.password("password123")
+		.passwordConfirm("password123")
+		.name("adminUser")
+		.type(AccountType.PERSONNEL)
+		.build();
+
+	public static final SignUpRequest TEST_DUPLICATED_EMAIL = SignUpRequest.builder()
+		.email("testuser@test.com")
 		.password("password123")
 		.passwordConfirm("password123")
 		.name("adminUser")
